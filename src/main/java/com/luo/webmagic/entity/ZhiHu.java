3329e193720ce004bd8e740afa1925d1af31c7d8
luoxuesen <luoxuesen@ysstech.com>
2018-12-24 13:44:38 +0800

    2%

--- src/main/java/com/luo/webmagic/entity/ZhiHu.java
+++
@@ -1,9 +0,0 @@
-package com.luo.webmagic.entity;
-
-import lombok.Data;
-
-@Data
-public class ZhiHu {
-    private String id;
-    private String url;
-}
--- src/main/java/com/luo/webmagic/service/FileService.java
+++
@@ -1,7 +0,0 @@
-package com.luo.webmagic.service;
-
-import java.util.List;
-
-public interface FileService {
-    void downloadPicture(List<String> list);
-}
